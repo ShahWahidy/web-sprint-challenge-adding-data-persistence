42f2089eca8d14a3ba3dc83323c4ea7c9090b6d2
Ahmadshah Wahidy <shah.wahidy@gmail.com>
2022-03-25 10:30:21 -0700

    completed sprinted challenge

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,23 @@
-// build your `/api/projects` router here
+const express = require("express");
+const Project = require("./model");
+const { validateBody } = require("./projectsMd");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Project.findAll().then((resp) => {
+    res.status(200).json(resp);
+  });
+});
+
+router.post("/", validateBody, (req, res) => {
+  Project.postProject(req.body)
+    .then((resp) => {
+      res.status(201).json(resp);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+
+module.exports = router;
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,22 @@
-// build your `/api/resources` router here
+const express = require("express");
+const { validateBody, checkResourceNameUnique } = require("./resourcesMd");
+const Resource = require("./model");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Resource.findAll().then((resp) => {
+    res.status(200).json(resp);
+  });
+});
+
+router.post("/", validateBody, checkResourceNameUnique, (req, res, next) => {
+  Resource.postResource(req.body)
+    .then((resp) => {
+      res.status(201).json(resp);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+module.exports = router;
